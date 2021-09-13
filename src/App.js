4ae7b60cd861b6bb9200376bf6192ef55971a159
daria888-hub <daria.lee365@gmail.com>
2021-09-13 13:54:03 +1000

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
        <h1>Hello</h1>
-       <p>Created by Daria Lee </p>
+       <p>Created by Daria Lee {"  "}
      < a href="https://github.com/Daria888-hub/Weather-2.0" target="_blank">Open sourse</a> 
-     
+     </p>
       </header>
+      </div>
     </div>
+    
   );
 }
 
