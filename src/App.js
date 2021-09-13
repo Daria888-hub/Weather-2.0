b9b6535477f8ed8b448c863bf2d1925fc9b2df77
daria888-hub <daria.lee365@gmail.com>
2021-09-13 13:40:48 +1000

    link added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
        <h1>Hello</h1>
-       
+       <p>Created by Daria Lee </p>
+     < a href="https://github.com/Daria888-hub/Weather-2.0" target="_blank">Open sourse</a> 
+     
       </header>
     </div>
   );
