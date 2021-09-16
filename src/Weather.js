d49452a72486dc98e7a048c6a5ca2d4720a367ab
daria888-hub <daria.lee365@gmail.com>
2021-09-16 13:49:23 +1000

    API search added

--- src/Weather.js
+++ src/Weather.js
@@ -1,47 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
 import axios from "axios";
  
 export default function Weather(){
+    
+    const [ weatherData, setWeatherData] = useState({ ready : false});
 
 function handleResponse(response){
     console.log(response.data)
+    
+
+    setWeatherData ( { 
+        ready:true,
+        date:"Monday 12:00",
+        tempreture:response.data.main.temp,
+        city: response.data.name,
+        humidity:response.data.main.humidity,
+        wind: response.data.wind.speed,
+        description: response.data.weather[0].description,
+        icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/Emoji_u2600.svg/192px-Emoji_u2600.svg.png"
+    })
 }
 
-    let city ="Brisbane"
-    const ApiKey ="869fd32af5911e8174d6e4840a9664b5";
-    const ApiUrl =`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${ApiKey}&units=metric`
-
-    axios.get(ApiUrl).then(handleResponse);
-
-    return( <div className= "weather">
+if (weatherData.ready){
+     return( <div className= "weather">
 
     <form > 
         <div className="row">   
         <div className="col-8">
-<input type="search" placeholder="enter the city" className="form-search"/>
+<input type="search" placeholder="enter the city" className="col-8"/>
 </div>
 <div className="col-3"> 
 <input type="submit" value="Search" className= "btn btn-primary"/>
 </div>
 </div>
 </form>
-        <h1>Brisbane</h1>
+        <h1>{weatherData.city}</h1>
         <ul>
-            <li>Monday 12:00</li>
-            <li>Sunny</li>
+            <li>{weatherData.date} </li>
+            <li>{weatherData.description}</li>
         </ul>
 <div className="row">
 <div className= "col-6">
     
-<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/Emoji_u2600.svg/192px-Emoji_u2600.svg.png" alt="20px"></img>
-6 C
+<img src= { weatherData.icon}></img>
+<span className="tempreture"> {Math.round(weatherData.tempreture)} </span> 
+<span className="unit"> C</span>
 </div>
 <div className= "col-6">
     <ul>
-        <li> Humidity = 50%</li>
-        <li>Wind = 100 km/h</li>
-        <li>Participation = 15%</li>
+        <li> Humidity = {weatherData.humidity}%</li>
+        <li>Wind = {weatherData.wind}  km/h</li>
+       
     </ul>
 
 </div>
@@ -50,4 +60,18 @@ function handleResponse(response){
 </div>
      </div>
     )
+
+}
+else { 
+     let city ="Brisbane"
+    const ApiKey ="869fd32af5911e8174d6e4840a9664b5";
+    const ApiUrl =`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${ApiKey}&units=metric`
+
+    axios.get(ApiUrl).then(handleResponse);
+    return ( "Loading...")
+
+}
+
+   
+   
 }
